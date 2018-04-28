73dffe1c7fbd224e19eb2c674d6af94aaff3530b
yuweijun <yuweijun@live.com>
2018-04-28 11:42:11 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -11,6 +11,13 @@ if grep -q "HOME/bin" ~/.bashrc; then
     echo "export PATH=\$HOME/bin:\$PATH" >> ~/.bashrc
 fi
 
+if [ ! -f ~/bin/greys ]; then
+    mkdir -p ~/bin/java
+    cp greys-anatomy/bin/greys.sh ~/bin/java
+    echo -e "#!/bin/bash\nJAVA_HOME=\${JAVA_HOME:-/usr/lib/jvm/java-8-oracle} ~/bin/java/greys.sh \$@" > ~/bin/greys
+    chmod a+x ~/bin/greys
+fi
+
 if [ ! -f ~/bin/decompiler ] && type ant 2>/dev/null; then
     cd fernflower-decompiler
     ant clean
@@ -20,15 +27,10 @@ if [ ! -f ~/bin/decompiler ] && type ant 2>/dev/null; then
     echo -e "#!/bin/bash\njava -jar ~/bin/java/fernflower.jar \$@" > ~/bin/decompiler
     chmod a+x ~/bin/decompiler
     cd ..
+elif [ -f ~/bin/decompiler ]; then
+    echo "decompiler file exists."
 elif type ant 2>/dev/null; then
     echo "ant command not found."
-fi
-
-if [ ! -f ~/bin/greys ]; then
-    mkdir -p ~/bin/java
-    cp greys-anatomy/bin/greys.sh ~/bin/java
-    echo -e "#!/bin/bash\nJAVA_HOME=\${JAVA_HOME:-/usr/lib/jvm/java-8-oracle} ~/bin/java/greys.sh \$@" > ~/bin/greys
-    chmod a+x ~/bin/greys
 fi
 
 if [ ! -f ~/bin/jd-cli ] && type mvn 2>/dev/null; then
