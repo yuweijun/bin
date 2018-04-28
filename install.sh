6f135e97873e2101a0cb537ef4242b00471bdc70
yuweijun <yuweijun@live.com>
2018-04-28 11:32:13 +0800

    add sslpoke for https sniff

--- install.sh
+++ install.sh
@@ -16,6 +16,8 @@ if [ ! -f ~/bin/decompiler ] && type ant 2>/dev/null; then
     echo -e "#!/bin/bash\njava -jar ~/bin/java/fernflower.jar \$@" > ~/bin/decompiler
     chmod a+x ~/bin/decompiler
     cd ..
+elif type ant 2>/dev/null; then
+    echo "ant command not found."
 fi
 
 if [ ! -f ~/bin/greys ]; then
@@ -23,4 +25,29 @@ if [ ! -f ~/bin/greys ]; then
     cp greys-anatomy/bin/greys.sh ~/bin/java
     echo -e "#!/bin/bash\nJAVA_HOME=\${JAVA_HOME:-/usr/lib/jvm/java-8-oracle} ~/bin/java/greys.sh \$@" > ~/bin/greys
     chmod a+x ~/bin/greys
+fi
+
+if [ ! -f ~/bin/jd-cli ] && type mvn 2>/dev/null; then
+    mkdir -p ~/bin/java
+    cd jd-cmd
+    mvn clean package
+    cp jd-cli/target/jd-cli.jar ~/bin/java
+    echo -e "#!/bin/bash\njava -jar ~/bin/java/jd-cli.jar \$@" > ~/bin/jd-cli
+    chmod a+x ~/bin/jd-cli
+elif [ -f ~/bin/jd-cli ]; then
+    echo "jd-cli file exists."
+elif type mvn 2>/dev/null; then
+    echo "mvn command not found."
+fi
+
+if [ ! -f ~/bin/sslpoke ] && type mvn 2>/dev/null; then
+    mkdir -p ~/bin/java
+    mvn clean package
+    cp target/bin.jar ~/bin/java
+    echo -e "#!/bin/bash\njava -cp ~/bin/java/bin.jar SSLPoke \$@" > ~/bin/sslpoke
+    chmod a+x ~/bin/sslpoke
+elif [ -f ~/bin/sslpoke ]; then
+    echo "sslpoke file exists."
+elif type mvn 2>/dev/null; then
+    echo "mvn command not found."
 fi
---
+++ src/main/java/SSLPoke.java
@@ -0,0 +1,44 @@
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * Establish a SSL connection to a host and port, writes a byte and
+ * prints the response. See
+ * http://confluence.atlassian.com/display/JIRA/Connecting+to+SSL+services
+ * <p>
+ * https://confluence.atlassian.com/download/attachments/117455/SSLPoke.java
+ * <p>
+ * java -Djavax.net.ssl.trustStore=/etc/java/conf/truststore.jks -Djavax.net.ssl.trustStorePassword=changeit SSLPoke httpbin.org 443
+ */
+public class SSLPoke {
+
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.out.println("Usage: " + SSLPoke.class.getName() + " <host> <port>");
+            System.exit(1);
+        }
+
+        try {
+            SSLSocketFactory sslsocketfactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
+            SSLSocket sslsocket = (SSLSocket) sslsocketfactory.createSocket(args[0], Integer.parseInt(args[1]));
+
+            InputStream in = sslsocket.getInputStream();
+            OutputStream out = sslsocket.getOutputStream();
+
+            // Write a test byte to get a reaction :)
+            out.write(1);
+
+            while (in.available() > 0) {
+                System.out.print(in.read());
+            }
+            System.out.println("Successfully connected");
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+
+}
+
