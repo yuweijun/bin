bb9c4e9dbf5a2bf20a33ce340aeb3d149fd8926e
yuweijun <yuweijun@live.com>
2018-05-16 00:40:00 +0800

    update os/macosx

--- os/macosx.sh
+++ os/macosx.sh
@@ -2,4 +2,15 @@
 
 set -x
 
+if type brew >/dev/null 2>&1; then
+    echo "brew installed"
+else
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+HOMEBREW_NO_AUTO_UPDATE=1
+
+brew cask install java8
+brew install gradle maven
+
 pip2 install pygments
