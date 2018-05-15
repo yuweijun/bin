1a124001b27541d664112b2df76bce67dcc2f7b5
yuweijun <yuweijun@live.com>
2018-05-15 22:35:52 +0800

    add jq json library

--- os/ubuntu.sh
+++ os/ubuntu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-apt install -y g++ gcc make xclip nodejs npm python3-venv python-pygments
+apt install -y g++ gcc make xclip nodejs npm python3-venv python-pygments jq
 
 npm install -g tldr
