b80b08b4713859fdbc707e7e0311459aebb2044c
yuweijun <yuweijun@live.com>
2018-05-15 21:13:49 +0800

    update scripts of os

--- os/centos7.sh
+++ os/centos7.sh
@@ -5,5 +5,5 @@ echo "centos7 softwares install..."
 yum install -y http://opensource.wandisco.com/centos/7/git/x86_64/wandisco-git-release-7-2.noarch.rpm
 yum update -y nss curl libcurl
 yum install -y git
-yum install -y tmux mycli the_silver_searcher
+yum install -y tmux mycli the_silver_searcher python-pygments
 
--- os/ubuntu.sh
+++ os/ubuntu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-apt install -y g++ gcc make xclip nodejs npm python3-venv
+apt install -y g++ gcc make xclip nodejs npm python3-venv python-pygments
 
 npm install -g tldr
