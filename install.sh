0f1e495ee8da459b8d18fa3f0fd83955f80f4665
yuweijun <yuweijun@live.com>
2018-05-17 00:37:27 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -1,31 +1,33 @@
 #!/bin/bash
 
-ARGV=$(getopt -o ir -l init,remote -- "$@")
+ARGV=$(getopt -o rif -l remote,init,force -- "$@")
 if [ $? != 0 ] ; then
     echo "getopt error"
 fi
 
 eval set -- "$ARGV"
 
+cd $(dirname "$0") || exit
+
+DIR="$(pwd)"
+
 INIT=false
 REMOTE=false
+FORCE=false
+DEST=$DIR/bin
 
 while true; do
   case "$1" in
-    -i | --init ) INIT=true; shift ;;
     -r | --remote ) REMOTE=true; shift ;;
+    -i | --init ) INIT=true; shift ;;
+    -f | --force ) FORCE=true; shift ;;
     -- ) shift; break ;;
     * ) break ;;
   esac
 done
 
-set -e
+# set -e
 # set -x
-
-cd $(dirname "$0") || exit
-
-DIR="$(pwd)"
-DEST=$HOME/bin
 
 if [ $UID -eq 0 ]; then
     DEST=/usr/local/bin
@@ -39,7 +41,7 @@ else
     if [ ! -e .git/modules ]; then
         git submodule update --init --recursive
     fi
-    echo "usage: ./install.sh [--init|--remote]"
+    echo "usage: ./install.sh [--init|--remote|--force]"
 fi
 
 mkdir -p ${DEST}/java
@@ -51,30 +53,37 @@ fi
 for f in $(ls ${DIR}/bash-files)
 do
     file=${DIR}/bash-files/${f}
-    cp "${file}" "${DEST}"
+    cp -r "${file}" "${DEST}"
 done
 
-if [ ! -f ${DEST}/greys ]; then
+if [ ! -f ${DEST}/greys ] || ${FORCE}; then
     cp greys-anatomy/bin/greys.sh ${DEST}/java
     echo -e "#!/bin/bash\nJAVA_HOME=\${JAVA_HOME:-/usr/lib/jvm/java-8-oracle} ${DEST}/java/greys.sh \$@" > ${DEST}/greys
     chmod a+x ${DEST}/greys
 fi
 
-if [ ! -f ${DEST}/decompiler ] && type ant 2>/dev/null; then
-    cd fernflower-decompiler || exit
-    ant clean
-    ant
-    cp fernflower.jar ${DEST}/java
-    echo -e "#!/bin/bash\njava -jar ${DEST}/java/fernflower.jar \$@" > ${DEST}/decompiler
-    chmod a+x ${DEST}/decompiler
-    cd -
-elif [ -f ${DEST}/decompiler ]; then
-    echo "decompiler file exists."
-elif type ant 2>/dev/null; then
-    echo "ant command not found."
+if ! type ant 2>/dev/null; then
+    echo "ant command not found"
+else
+    if [ ! -f ${DEST}/decompiler ] || $FORCE; then
+        cd fernflower-decompiler || exit
+        ant clean
+        ant
+        cp fernflower.jar ${DEST}/java
+        echo -e "#!/bin/bash\njava -jar ${DEST}/java/fernflower.jar \$@" > ${DEST}/decompiler
+        chmod a+x ${DEST}/decompiler
+        cd -
+    elif [ -f ${DEST}/decompiler ]; then
+        echo "decompiler file exists."
+    fi
 fi
 
-if [ ! -f ${DEST}/jd-cli ] && type mvn 2>/dev/null; then
+if ! type mvn 2>/dev/null; then
+    echo "mvn command not found"
+    exit 0
+fi
+
+if [ ! -f ${DEST}/jd-cli ] || ${FORCE}; then
     cd jd-cmd || exit
     mvn clean package
     cp jd-cli/target/jd-cli.jar ${DEST}/java
@@ -83,22 +92,18 @@ if [ ! -f ${DEST}/jd-cli ] && type mvn 2>/dev/null; then
     cd -
 elif [ -f ${DEST}/jd-cli ]; then
     echo "jd-cli file exists."
-elif type mvn 2>/dev/null; then
-    echo "mvn command not found."
 fi
 
-if [ ! -f ${DEST}/sslpoke ] && type mvn 2>/dev/null; then
+if [ ! -f ${DEST}/sslpoke ] || ${FORCE}; then
     mvn clean package
     cp target/bin.jar ${DEST}/java
     echo -e "#!/bin/bash\njava -cp ${DEST}/java/bin.jar SSLPoke \$@" > ${DEST}/sslpoke
     chmod a+x ${DEST}/sslpoke
 elif [ -f ${DEST}/sslpoke ]; then
     echo "sslpoke file exists."
-elif type mvn 2>/dev/null; then
-    echo "mvn command not found."
 fi
 
-if [ ! -f ${DEST}/sjk ] && type mvn 2>/dev/null; then
+if [ ! -f ${DEST}/sjk ] || ${FORCE}; then
     cd jvm-tools || exit
     mvn clean package
     cp sjk/target/sjk-*-SNAPSHOT.jar ${DEST}/java/sjk.jar
@@ -107,7 +112,5 @@ if [ ! -f ${DEST}/sjk ] && type mvn 2>/dev/null; then
     cd -
 elif [ -f ${DEST}/sjk ]; then
     echo "sjk file exists."
-elif type mvn 2>/dev/null; then
-    echo "mvn command not found."
 fi
 
