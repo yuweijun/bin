ba78f39afab6636f585b9994ff90fea68758468e
yuweijun <yuweijun@live.com>
2018-11-28 09:54:19 +0800

    add install compile flag

--- install.sh
+++ install.sh
@@ -12,6 +12,12 @@ if ! type mvn 2>/dev/null; then
     exit 0
 fi
 
+COMPILE=false
+
+if [ $# -gt 0 ]; then
+    COMPILE=true
+fi
+
 git submodule update --init --recursive
 
 if [ -e /usr/libexec/java_home ]; then
@@ -41,53 +47,61 @@ if [ 'arthas' ]; then
 fi
 
 if [ 'greys' ]; then
-    cp greys-anatomy/bin/greys.sh ${DEST}/java
     echo -e "#!/bin/bash\nexport JAVA_HOME=${JAVA_HOME}\n\${HOME}/bin/java/greys.sh \$@" > ${DEST}/greys
     chmod a+x ${DEST}/greys
 fi
 
-if ! type ant 2>/dev/null; then
-    echo "ant command not found"
-else
-    if [ 'decompiler' ]; then
-        cd fernflower-decompiler || exit
-        ant clean
-        ant
-        cp fernflower.jar ${DEST}/java
-        echo -e "#!/bin/bash\njava -jar \${HOME}/bin/java/fernflower.jar \$@" > ${DEST}/decompiler
-        chmod a+x ${DEST}/decompiler
-        cd -
+if [ 'decompiler' ]; then
+    if $COMPILE; then
+        if ! type ant 2>/dev/null; then
+            cd fernflower-decompiler || exit
+            ant clean
+            ant
+            cp fernflower.jar ${DEST}/java
+            cd -
+        fi
     fi
+
+    echo -e "#!/bin/bash\njava -jar \${HOME}/bin/java/fernflower.jar \$@" > ${DEST}/decompiler
+    chmod a+x ${DEST}/decompiler
 fi
 
 if [ 'jd-cli' ]; then
-    cd jd-cmd || exit
-    mvn -Dmaven.test.skip=true clean package
-    cp jd-cli/target/jd-cli.jar ${DEST}/java
+    if $COMPILE; then
+        cd jd-cmd || exit
+        mvn -Dmaven.test.skip=true clean package
+        cp jd-cli/target/jd-cli.jar ${DEST}/java
+        cd -
+    fi
+
     echo -e "#!/bin/bash\njava -jar \${HOME}/bin/java/jd-cli.jar \$@" > ${DEST}/jd-cli
     chmod a+x ${DEST}/jd-cli
-    cd -
 fi
 
 if [ 'sjk' ]; then
-    cd jvm-tools || exit
-    mvn -Dmaven.test.skip=true clean package
-    cp sjk/target/sjk-*-SNAPSHOT.jar ${DEST}/java/sjk.jar
+    if $COMPILE; then
+        cd jvm-tools || exit
+        mvn -Dmaven.test.skip=true clean package
+        cp sjk/target/sjk-*-SNAPSHOT.jar ${DEST}/java/sjk.jar
+        cd -
+    fi
+
     echo -e "#!/bin/bash\njava -jar \${HOME}/bin/java/sjk.jar \$@" > ${DEST}/sjk
     chmod a+x ${DEST}/sjk
-    cd -
 fi
 
 if [ 'btrace' ]; then
-    cd btrace
-    git checkout -b v1.3.11.1
-    ./gradlew build -x test -x javadoc
+    if $COMPILE; then
+        cd btrace
+        git checkout v1.3.11.1
+        ./gradlew build -x test -x javadoc
+        cp bin/btrace ${DEST}/java/btrace
+        cp build/btrace-*.jar ${DEST}/java/build
+        cd -
+    fi
 
-    cp bin/btrace ${DEST}/java/btrace
-    cp build/btrace-*.jar ${DEST}/java/build
     echo -e "#!/bin/bash\nexport JAVA_HOME=${JAVA_HOME}\nexport BTRACE_HOME=\${HOME}/bin/java\n\${HOME}/bin/java/btrace \$@" > ${DEST}/btrace
     chmod a+x ${DEST}/btrace
-    cd -
 fi
 
 for f in $(ls ${DEST})
