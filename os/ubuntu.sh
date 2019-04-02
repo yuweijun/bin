9dbbd26573213b93b949e8aa0a6f4524e30f5324
yuweijun <yuweijun@live.com>
2019-04-02 09:30:17 +0800

    add hsdis-amd64.so for java8 on linux

--- os/ubuntu.sh
+++ os/ubuntu.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+git clone https://github.com/yuweijun/hsdis
+
+cd hsdis
+tar -zxvf binutils-2.26.tar.gz
+make BINUTILS=binutils-2.26 ARCH=amd64
+
+sudo cp build/linux-amd64/hsdis-amd64.so /usr/lib/jvm/java-8-oracle/jre/lib/amd64/server/
 
