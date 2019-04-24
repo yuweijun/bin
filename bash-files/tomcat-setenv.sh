904c998754152c994daf0a6a28af359cb181e6d1
yuweijun <yuweijun@live.com>
2019-04-24 10:35:16 +0800

    add tomcat setenv.sh

--- bash-files/tomcat-setenv.sh
+++ bash-files/tomcat-setenv.sh
@@ -2,4 +2,4 @@
 
 UMASK="0022"
 
-JAVA_OPTS="$JAVA_OPTS -Dspring.profiles.active=prod -Dfile.encoding=UTF-8 -Xmx1500m -Xms1500m -XX:+PrintTenuringDistribution -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCTimeStamps -verbose:gc -Xloggc:${CATALINA_BASE}/logs/gc-$(date +%Y-%m-%d).log -XX:GCLogFileSize=100M -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=5 -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=${CATALINA_BASE}/logs/heapdump.hprof"
+JAVA_OPTS="$JAVA_OPTS -Dspring.profiles.active=prod -Dfile.encoding=UTF-8 -Xmx1500m -Xms1500m -XX:+PrintCommandLineFlags -XX:+PrintTenuringDistribution -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCTimeStamps -verbose:gc -Xloggc:${CATALINA_BASE}/logs/gc-$(date +%Y-%m-%d).log -XX:GCLogFileSize=100M -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=5 -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=${CATALINA_BASE}/logs/heapdump.hprof"
